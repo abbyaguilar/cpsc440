7f83c1dcb8adad170db4978680946a29d0977d2e
abbyaguilar <111159835+abbyaguilar@users.noreply.github.com>
2023-04-30 07:03:01 -0700

    Create test.py

---
+++ test.py
@@ -0,0 +1,6 @@
+import os
+xml_path = 'haarcascade_frontalface_default.xml'
+if os.path.isfile(xml_path):
+    face_cascade = cv2.CascadeClassifier(xml_path)
+else:
+    print(f"Could not find XML file: {xml_path}")
