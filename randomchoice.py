c3de5215457b9ebdbb1e0d07446e927e4c9026b0
abbyaguilar <111159835+abbyaguilar@users.noreply.github.com>
2023-04-30 21:29:10 -0700

    Update randomchoice.py

--- randomchoice.py
+++ randomchoice.py
@@ -1,16 +1,17 @@
 import os
 import random
 
-def choose_random_image(cpsc440):
     # Create a list to store image filenames
     image_files = []
     
+    path = "/Users/abigailaguilar/Desktop/cpsc440"
+    
     # Loop through all files in the directory
-    for images in os.listdir(cpsc440):
+    for images in os.listdir(path):
         # Check if the file is an image file
         if filename.endswith(".png") or filename.endswith(".jpg"):
             # If it is, add the filename to the list
-            image_files.append(cpsc440)
+            image_files.append()
     
     # If no image files are found, return None
     if not image_files:
@@ -18,15 +19,6 @@ def choose_random_image(cpsc440):
     
     # Choose a random image from the list
     random_image_filename = random.choice(image_files)
+    print(random_image_filename) 
     
-    # Return the full path to the random image
-    return os.path.join(directory, random_image_filename)
-
-# Choose a random image from the specified directory
-random_image_path = choose_random_image("./cpsc440/")
-
-# If an image was found, print its path
-if random_image_path:
-    print("Random image:", random_image_path)
-else:
-    print("No image files found.")
+ 
