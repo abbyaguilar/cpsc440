3d2b08bc9c1765ee03b53df4faadaf0ce6128f48
abbyaguilar <111159835+abbyaguilar@users.noreply.github.com>
2023-05-01 12:07:16 -0700

    Update randomchoice.py

--- randomchoice.py
+++ randomchoice.py
@@ -1,1 +1,10 @@
-print("3.png")
+import cv2
+import os
+
+def load_images_from_folder(cpsc440):
+    images = []
+    for filename in os.listdir(cpscc440):
+        img = cv2.imread(os.path.join(cpsc440,filename))
+        if img is not None:
+            images.append(img)
+    return images
