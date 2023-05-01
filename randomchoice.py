0b36628a5ed81f6f5eb16a60db68f4635b913623
abbyaguilar <111159835+abbyaguilar@users.noreply.github.com>
2023-04-30 21:42:42 -0700

    Update randomchoice.py

--- randomchoice.py
+++ randomchoice.py
@@ -1,22 +1,4 @@
 import os
 import random
 
-# Create a list to store image filenames
-image_files = []
-
-path = "/Users/abigailaguilar/cpsc440"
-
-# Loop through all files in the directory
-for filename in os.listdir(path):
-    # Check if the file is an image file
-    if filename.endswith(".png") or filename.endswith(".jpg"):
-        # If it is, add the filename to the list
-        image_files.append(filename)
-    
-# If no image files are found, return None
-if not image_files:
-    print("No image files found.")
-else:
-    # Choose a random image from the list
-    random_image_filename = random.choice(image_files)
-    print(random_image_filename)
+    print("2.png")
