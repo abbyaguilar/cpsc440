2d7a1ba0e77a70c5cbe44130342179b50a69b701
abbyaguilar <111159835+abbyaguilar@users.noreply.github.com>
2023-05-02 00:56:48 -0700

    Update randomchoice.py

--- randomchoice.py
+++ randomchoice.py
@@ -1,10 +1,23 @@
-import cv2
 import os
+import random
+import cv2
 
-def load_images_from_folder(cpsc440):
-    images = []
-    for filename in os.listdir(cpscc440):
-        img = cv2.imread(os.path.join(cpsc440,filename))
-        if img is not None:
-            images.append(img)
-    return images
+folder_path = "/home/abbys/cpsc440/cpsc440"
+file_list = [f for f in os.listdir(folder_path) if f.endswith(".png")]
+
+if len(file_list) > 0:
+    # Choose a random image file from the folder
+    file_path = os.path.join(folder_path, random.choice(file_list))
+
+    # Load the image using OpenCV
+    image = cv2.imread(file_path)
+
+    # Display the image
+    cv2.imshow("Random Image", image)
+    cv2.waitKey(0)
+
+    # Clean up
+    cv2.destroyAllWindows()
+
+else:
+    print("No .png files found in the specified folder path.")
